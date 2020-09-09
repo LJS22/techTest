fe0d477121e35ed46e68018b772905fe12bdd883
LJS22 <LJS22@users.noreply.github.com>
2020-09-09 12:55:38 +0100

    Updates

---
+++ precache-manifest.81be846a2ae5e22bcf8322600c5f471b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ae80ab0cbe4c17e7e86e3e8a7c3e5a",
+    "url": "/techTest/index.html"
+  },
+  {
+    "revision": "d36998234d704688f6ac",
+    "url": "/techTest/static/css/main.8305fed3.chunk.css"
+  },
+  {
+    "revision": "ed26acb9477468d12155",
+    "url": "/techTest/static/js/2.8927583d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/techTest/static/js/2.8927583d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36998234d704688f6ac",
+    "url": "/techTest/static/js/main.db9e55bf.chunk.js"
+  },
+  {
+    "revision": "9e5a4cb897ca37939cbb",
+    "url": "/techTest/static/js/runtime-main.bf4e9322.js"
+  }
+]);
